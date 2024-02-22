f77f0dcbcc34c1c2a2c149008b8b477ada4f4898
Ammar Mirkar <69316553+Ammar-Mirkar@users.noreply.github.com>
2024-02-22 20:47:49 +0530

    Update Find all the Distribution Groups a user is a member of with PowerShell.ps1

--- Find all the Distribution Groups a user is a member of with PowerShell.ps1
+++ Find all the Distribution Groups a user is a member of with PowerShell.ps1
@@ -1,3 +1,3 @@
-$Username = "request@speridian.com"
+$Username = "request@contoso.com"
 $DistributionGroups= Get-DistributionGroup | where { (Get-DistributionGroupMember $_.Name | foreach {$_.PrimarySmtpAddress}) -contains "$Username"}
 $DistributionGroups | clip
